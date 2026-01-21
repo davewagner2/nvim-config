6724d5d13aa39eb3c612e7ab2c484e5bf6e78281
dave wagner <dave.wagner@gmail.com>
2026-01-21 15:24:33 -0600

    disable autocomplete on <CR> without selecting entry

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+          },
+        },
+      },
+    },
+  },
+}
+
