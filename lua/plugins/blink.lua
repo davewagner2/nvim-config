23ec3209eb563a578e6f515ccd9202242e6a870d
dave wagner <dave.wagner@gmail.com>
2026-01-22 19:54:37 -0600

    Make blink not auto-select first, but jump to it on TAB

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,16 +1,32 @@
-return {
-  {
-    "saghen/blink.cmp",
-    optional = true,
-    opts = {
-      completion = {
-        list = {
-          selection = {
-            preselect = false,
-          },
+return { -- override blink.cmp plugin to keep from autoselecing and gets <TAB> to rotate through options
+  "Saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
         },
       },
+    },
+    keymap = {
+      ["<Tab>"] = { "select_next", "fallback" },
     },
   },
 }
 
+-- option to disable preselect entirely
+-- return {
+--   {
+--     "saghen/blink.cmp",
+--     optional = true,
+--     opts = {
+--       completion = {
+--         list = {
+--           selection = {
+--             preselect = false,
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
