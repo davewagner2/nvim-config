86cec2d0f30643d5026c30ab8df4d03462ee52a7
davewagner2 <dave.wagner@gmail.com>
2026-09-09 14:10:43 -0500

    Adding codecompanion for LLM
    Add codecompanion configured to use my VPS hosted ollama running a small
    qwen coder instance

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,38 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("codecompanion").setup({
+      adapters = {
+        http = {
+          ollama = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              env = {
+                url = "https://coder.ak4go.com",
+              },
+              headers = {
+                ["Content-Type"] = "application/json",
+                ["CF-Access-Client-Id"] = os.getenv("CF_ACCESS_CLIENT_ID"),
+                ["CF-Access-Client-Secret"] = os.getenv("CF_ACCESS_CLIENT_SECRET"),
+              },
+              parameters = {
+                sync = true,
+              },
+            })
+          end,
+        },
+      },
+      interactions = {
+        chat = {
+          adapter = "ollama",
+        },
+        inline = {
+          adapter = "ollama",
+        },
+      },
+    })
+  end,
+}
